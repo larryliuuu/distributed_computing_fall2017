ce3d93d00d07ad64b63b5a702de7da7e06f22033
Suriya Kodeswaran <suriyakode@Suriyas-MacBook-Pro.local>
2017-11-10 12:07:04 -0600

    adding request and response manager

---
+++ request.py
@@ -0,0 +1,29 @@
+import requests
+
+
+IP = '127.0.0.1'
+REQUEST_PORT = '5434'
+APP_NAME = 'distributed_computing/0.1'
+
+def READ(key):
+	payload = {'key': key}
+	headers = {'user-agent': APP_NAME}
+	r = requests.get("http://" + IP + ":" + REQUEST_PORT, params=payload, headers=headers)
+	print r.text
+
+
+def WRITE(key):
+	payload = {'key': key}
+	headers = {'user-agent': APP_NAME}
+	r = requests.post("http://" + IP + ":" + REQUEST_PORT, params=payload, headers=headers)
+	print r.text
+
+
+def DELETE(key):
+	payload = {'key': key}
+	headers = {'user-agent': APP_NAME}
+	r = requests.delete("http://" + IP + ":" + REQUEST_PORT, params=payload, headers=headers)
+	print r.text
+
+
+DELETE('lol')
