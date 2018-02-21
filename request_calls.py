47433e1e1144768bad3af0ad27b80040b92a847b
Larry Liu <larryliu96@gmail.com>
2018-02-20 23:04:45 -0600

    created output logs for request_calls

--- request_calls.py
+++ request_calls.py
@@ -9,16 +9,16 @@ def READ(dst, key, vt):
 	payload = {'key': key}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.get("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
-	print r.text
+	print str(r.status_code) + ":" + r.text
 
 def WRITE(dst, key, value, host, vt):
 	payload = {'key': key, 'value': value, 'host': host}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.post("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
-	print r.status_code
+	print str(r.status_code) + ":" + key + ":" + value
 
 def DELETE(dst, key, vt):
 	payload = {'key': key}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.delete("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
-	print r.status_code
+	print str(r.status_code) + ":" + key
