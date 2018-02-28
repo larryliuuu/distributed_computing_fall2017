9b4ae02742ed19d3e13c5e88e3a561be728d549c
Larry Liu <larryliu96@gmail.com>
2018-02-27 19:19:20 -0600

    added version param

--- request_calls.py
+++ request_calls.py
@@ -5,13 +5,13 @@ import requests
 SERVER_PORT = '5434'
 APP_NAME = 'distributed_computing/0.1'
 
-def READ(dst, key, vt):
+def READ(dst, key, vt, version):
 	payload = {'key': key, 'version': version}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.get("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
 	print str(r.status_code) + ":" + r.text
 
-def WRITE(dst, key, value, host, vt):
+def WRITE(dst, key, value, host, vt, version):
 	payload = {'key': key, 'value': value, 'host': host, 'version': version}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.post("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
