11fa55fe694a93d0cef4740acfd65766af3585e9
Larry Liu <larryliu96@gmail.com>
2018-02-27 19:15:02 -0600

    add version to payload

--- request_calls.py
+++ request_calls.py
@@ -6,13 +6,13 @@ SERVER_PORT = '5434'
 APP_NAME = 'distributed_computing/0.1'
 
 def READ(dst, key, vt):
-	payload = {'key': key}
+	payload = {'key': key, 'version': version}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.get("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
 	print str(r.status_code) + ":" + r.text
 
 def WRITE(dst, key, value, host, vt):
-	payload = {'key': key, 'value': value, 'host': host}
+	payload = {'key': key, 'value': value, 'host': host, 'version': version}
 	headers = {'user-agent': APP_NAME, 'vt': vt}
 	r = requests.post("http://" + dst + ":" + SERVER_PORT, params=payload, headers=headers)
 	print str(r.status_code) + ":" + key + ":" + value
